f50dcc98e8f7177c3031d901a0413d3c62ea53ee
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-26 22:16:26 +0200

    Format and change size

--- cute.js
+++ cute.js
@@ -2,39 +2,39 @@
 
 const imgUrl = "https://maas.filiptronicek.now.sh/r/kittens";
 
-const imgReq = await new Request(imgUrl)
-const img = await imgReq.loadImage()
+const imgReq = await new Request(imgUrl);
+const img = await imgReq.loadImage();
 
 if (config.runsInWidget) {
   // create and show widget
-  let widget = createWidget(img)
-  Script.setWidget(widget)
-  Script.complete()
+  let widget = createWidget(img);
+  Script.setWidget(widget);
+  Script.complete();
 } else {
-  Safari.open(imgUrl)
+  Safari.open(imgUrl);
 }
 
 function createWidget(img) {
-  let widget = new ListWidget()
-  widget.backgroundColor = new Color("#ffffff")
-  widget.url = imgUrl
-  let image = widget.addImage(img)
-  image.centerAlignImage()
-  const [width, height] = getDimensions()
-  image.imageSize = new Size(width, height)
-  return widget
+  let widget = new ListWidget();
+  widget.backgroundColor = new Color("#ffffff");
+  widget.url = imgUrl;
+  let image = widget.addImage(img);
+  image.centerAlignImage();
+  const [width, height] = getDimensions();
+  image.imageSize = new Size(width, height);
+  return widget;
 }
 
 function getDimensions() {
-  const size = args.widgetParameter || 1
+  const size = args.widgetParameter || 1;
   switch (Number(size)) {
     case 1:
-      return [470, 470]
+      return [250, 250];
     case 2:
-      return [330, 180]
+      return [330, 180];
     case 4:
-      return [330, 330]
+      return [330, 330];
     default:
-      return [170, 170]
+      return [170, 170];
   }
 }
